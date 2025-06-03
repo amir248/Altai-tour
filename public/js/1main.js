cfe17b1f9d0c0d0987010627c08b256d754a55d6
chikchicly <chikchicly@gmail.com>
2025-06-03 18:39:36 +0700

    tawk

--- public/js/1main.js
+++ public/js/1main.js
@@ -122,11 +122,29 @@ function sendNow(){
             resolve(send());
         },377);
     })};
+
+    function tawk(){
+        return new Promise((resolve)=>{
+            function goTawk(){
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/683ed4ce945f2a1912c9ad09/1isqmuhtf';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();
+            }
+          resolve(goTawk());
+        });
+    };//tawk
 async function main(){
     // await first();
     await include();
     await openMenu();
     await sendNow();
     await yearDate();
+    await tawk();
 }
 main();
